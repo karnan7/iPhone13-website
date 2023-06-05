9beb73e069d9a5e6cdf6878adee75d752c95c0cc
karnan7 <adithkayalad@gmail.com>
2023-06-05 17:09:35 +0530

    firstpage animation

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-const tlH = gsap.
+const tlIntro = gsap.timeline({
+    scrollTrigger:{
+        trigger: '.first-page',
+        start:"0%",
+        end:"100%",
+        pin:true,
+        pinSpacing:false,
+    }
+})
+
+const tlH = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: { startColor:"blue", endColor:"blue" },
+        scrub: true,
+        start: "-40%",
+        end: "40%",
+    },
+})
+
+tlH.fromTo(".highlight", 
+{ color: "rgba(255, 255, 255, 0.4)" },
+{ color: "rgba(255, 255, 255, 1)", stagger: 1 },
+)
+
+const tlHRemove = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: {startColor:"blue", endColor:"blue"},
+        scrub:true,
+        start:"-20%",
+        end:"60%",
+    }
+})
+
+tlHRemove.to(".highlight", { color: "rgba(255, 255, 255, 0.4)", stagger: 1 });
