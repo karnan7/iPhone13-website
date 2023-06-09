2d1f09850da1fc9ae7a7412b76825dd9f2e16bf1
karnan7 <adithkayalad@gmail.com>
2023-06-09 21:33:17 +0530

    completed animation

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const tlH = gsap.timeline({
         start: "-40%",
         end: "40%",
         scrub: true,
-        markers: { startColor: "blue", endColor: "blue"}
     }
 })
 
@@ -43,11 +42,10 @@ const tlSplit = gsap.timeline({
         start: "-10%",
         end: "25%",
         scrub: true,
-        markers: true,
     }
 })
 
-tlSplit.fromTo(".large-phone", { x: "40%" }, { x: "20%" },)
+tlSplit.fromTo(".large-phone", { x: "40%" }, { x: "20%" })
 
 tlSplit.fromTo( ".small-phone", { x: "-40%"}, { x: "-20%"}, "<" )
 
@@ -66,7 +64,7 @@ tlSplit.fromTo(
 const tlSplitPin = gsap.timeline({
     scrollTrigger: {
         trigger: ".third-page",
-        start: "10%",
+        start: "20%",
         end: "100%",
         pin: true,
         pinSpacing: false,
@@ -91,9 +89,45 @@ swatches.forEach((swatch, index) => {
         if(currentSwatch === swatchName) return
         
         gsap.set(closeUp, {zIndex:topIndex})
-        gsap.fromTo(closeUp, {opacity: 0}, {opacity: 1, duration: 1})
+        gsap.fromTo(closeUp, {opacity: 0}, {opacity: 1, duration: 1 })
+
+        gsap.to(gallery, { x:-co, duration: 1, ease:"Power2.easeOut" })
 
         topIndex++
         currentSwatch = swatchName ;
     })
 })
+
+// fifth-page
+
+const tlVideo = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".fifth-page",
+        start: "0%",
+        end: "150%",
+        scrub: true,
+        pin: true,
+    }
+})
+
+tlVideo.fromTo(".product-video", { currentTime: 0 }, { currentTime: 3 , duration: 1 })
+tlVideo.fromTo(".product-info-container h3",
+    { opacity: 0 },
+    {opacity: 1, duration:0.5, stagger: 0.25 }, "<"
+)
+
+// sixth page
+
+const tlParallax = gsap.timeline({
+    scrollTrigger : {
+        trigger: ".sixth-page",
+        start: "-25%",
+        end: "50%",
+        scrub: true,
+    }
+})
+
+
+tlParallax.fromTo(".photo-description", { y: 0 }, { y: -70 })
+tlParallax.fromTo(".portrait-container", { y: 0 }, { y: -80 }, "<");
+tlParallax.fromTo(".phone-video", { y: 0 }, { y: -200 }, "<");
